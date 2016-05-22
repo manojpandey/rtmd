b436cdf83a7dbd8c4119fc5a4749089abb51d089
Manoj Pandey <manojpandey1996@gmail.com>
2016-05-22 19:58:48 +0530

    multiple files

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 
@@ -10,8 +9,23 @@ app.use(express.static(__dirname + '/public'));
 
 // routes for app
 app.get('/', function(req, res) {
-  res.render('pad');
+    res.render('pad');
 });
+app.get('/(:id)', function(req, res) {
+    res.render('pad');
+});
+
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for sharejs 
+var options = {
+    db: { type: 'redis' },
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
